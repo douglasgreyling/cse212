c98acf777b2d6ee4900cef75d8e89e8236efff5b
Douglas Greyling <greyling.douglas@gmail.com>
2025-08-11 18:40:02 +0200

    crimesjohnson: Finish activity

--- week06/code/BinarySearchTree.cs
+++ week06/code/BinarySearchTree.cs
@@ -80,7 +80,17 @@ public class BinarySearchTree : IEnumerable<int>
 
     private void TraverseBackward(Node? node, List<int> values)
     {
-        // TODO Problem 3
+        if (node is not null)
+        {
+            // Visit right subtree first (largest values)
+            TraverseBackward(node.Right, values);
+
+            // Visit current node
+            values.Add(node.Data);
+
+            // Visit left subtree (smaller values)
+            TraverseBackward(node.Left, values);
+        }
     }
 
     /// <summary>
